4d59f960fe8cede5c750dd85ea7309c2b24056b0
Qishen Zhang <qishen23@gmail.com>
2021-04-27 13:47:57 -0500

    Update script for running ddlog benchmark.

--- solutions/differential-datalog/snq2/run.sh
+++ solutions/differential-datalog/snq2/run.sh
@@ -1,15 +1,15 @@
-
 #!/bin/bash
 
 # For compiled ddlog binary
-# export DDLOG_HOME=/home/qishen/.local/bin/ddlog
+# export DDLOG_HOME=$HOME/.local/bin/ddlog
 
-# For ddlog compiled from source
-export DDLOG_HOME=/home/qishen/differential-datalog
+# For ddlog compiled from source and change the path if the source code is in a different directory.
+export DDLOG_HOME=$HOME/differential-datalog
 
 # Create runtime for socialnetwork domain
 ddlog -i snq2.dl && 
 # Open the folder and build the runtime in Rust
 (cd snq2_ddlog && cargo build --release) #&& 
+
 # Run the program that depends on the runtime as a library with model as its parameter. 
-#(cd snq1_lib && cargo test --release -- it_works /home/qishen/ttc2018liveContest/models/512/ --nocapture)
+#(cd snq1_lib && cargo test --release -- it_works $HOME/ttc2018liveContest/models/512/ --nocapture)
